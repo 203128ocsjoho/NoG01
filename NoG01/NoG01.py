e538e3420a2cb2a7af4c1366b7152db903cb59fb
BuildTools <unconfigured@null.spigotmc.org>
2022-10-17 14:30:22 +0900

    Before Clone Push

--- NoG01/NoG01.py
+++ NoG01/NoG01.py
@@ -1,4 +1,52 @@
-x = 1;
-y = 2;
-z = (x + y);
-print(z);
+#tkinterのインポート
+import tkinter
+
+#tkinterを起動
+root = tkinter.Tk()
+#タイトルの設定
+root.title("メインメニュー")
+#画面サイズの指定
+root.geometry("500x500")
+
+def close_window():
+    root.destroy()
+
+# ラベル表示
+label = tkinter.Label(root, text="xxxx ツール", font=("MSゴシック", "20", "bold"))
+label.grid()
+
+# ボタンの設定(text=ボタンに表示するテキスト)
+btn0 = tkinter.Button(root, text='XXXXXX1',
+width = 50,
+height = 2,
+bg = "White",
+)
+btn1 = tkinter.Button(root, text='XXXXXX2',
+width = 50,
+height = 2,
+bg = "White",
+)
+btn2 = tkinter.Button(root, text='XXXXXX3',
+width = 50,
+height = 2,
+bg = "White",
+)
+btn3 = tkinter.Button(root, text='XXXXXX4',
+width = 50,
+height = 2,
+bg = "White",
+)
+btn_close = tkinter.Button(root, text='閉じる',
+width = 20,
+height = 2,
+bg = "White",
+command = close_window
+)
+#ボタンを配置する位置の設定
+btn0.place(x=20, y=60)
+btn1.place(x=20, y=120)
+btn2.place(x=20, y=180)
+btn3.place(x=20, y=240)
+btn_close.place(x=325, y=450)
+
+root.mainloop()
